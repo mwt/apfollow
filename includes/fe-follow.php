f3df5cb73745950db3979422d9d2e9f03e371208
Matthew W. Thomas <matthew@mwt.me>
2023-06-24 10:32:02 -0500

    more space in template

--- includes/fe-follow.php
+++ includes/fe-follow.php
@@ -4,32 +4,36 @@
 <head>
     <meta charset='utf-8'>
     <meta content='width=device-width, initial-scale=1' name='viewport'>
-    <title>Follow <?=(isset($local_fullname)) ? $local_fullname : $local_user ?></title>
+    <title>Follow <?= (isset($local_fullname)) ? $local_fullname : $local_user ?></title>
     <meta content='#212529' name='theme-color'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <style>.follow-container{max-width: 425px}</style>
+    <style>
+        .follow-container {
+            max-width: 425px
+        }
+    </style>
 </head>
 
 <body class="bg-dark text-light">
     <div class="container follow-container">
         <h2 class="text-center my-3 fs-5">You are going to follow:</h2>
         <div class="card bg-dark">
-            <a class="text-decoration-none" target="_blank" rel="noopener noreferrer" href="<?=$local_identifier ?>">
+            <a class="text-decoration-none" target="_blank" rel="noopener noreferrer" href="<?= $local_identifier ?>">
 <?php if (isset($local_image)) { ?>
-                <img class="card-img-top d-none d-sm-block" alt="" src="<?=$local_image ?>">
+                <img class="card-img-top d-none d-sm-block" alt="" src="<?= $local_image ?>">
 <?php } ?>
                 <div class="card-header d-flex">
-                    <img alt="" class="rounded" src="<?=(isset($local_icon)) ? $local_icon : $ini_array["placeholder_image"] ?>" width="48" height="48">
+                    <img alt="" class="rounded" src="<?= (isset($local_icon)) ? $local_icon : $ini_array["placeholder_image"] ?>" width="48" height="48">
                     <div class="mx-3">
-                        <strong class="p-name text-light"><?=(isset($local_fullname)) ? $local_fullname : $local_user ?></strong>
-                        <span class="d-block text-light text-opacity-50">@<?=$local_user ?>@<?=$local_instance ?></span>
+                        <strong class="p-name text-light"><?= (isset($local_fullname)) ? $local_fullname : $local_user ?></strong>
+                        <span class="d-block text-light text-opacity-50">@<?= $local_user ?>@<?= $local_instance ?></span>
                     </div>
                 </div>
             </a>
             <div class="card-body">
                 <form novalidate="novalidate" action="" enctype="multipart/form-data" accept-charset="UTF-8" method="post">
                     <div class="mb-3">
-                        <input type="hidden" name="remote_follow[local_id]" value="<?=$local_identifier ?>">
+                        <input type="hidden" name="remote_follow[local_id]" value="<?= $local_identifier ?>">
                         <input type="text" name="remote_follow[acct]" class="form-control bg-dark text-light border-secondary" placeholder="Enter your username@domain you want to follow from" autocapitalize="none" autocorrect="off">
                     </div>
                     <button type="submit" class="btn btn-primary w-100 text-uppercase">Proceed to follow</button>
@@ -37,7 +41,7 @@
             </div>
             <div class="card-footer">
                 <p class="card-text text-light text-opacity-50 text-center lh-sm">
-                    <small>Why is this step necessary? <code><?=$local_instance ?></code> might not be the server where you are registered, so we need to redirect you to your home server first.</small>
+                    <small>Why is this step necessary? <code><?= $local_instance ?></code> might not be the server where you are registered, so we need to redirect you to your home server first.</small>
                 </p>
             </div>
         </div>
